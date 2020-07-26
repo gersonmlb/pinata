c4d53ed4554e1558ec8b24e52c39ad4bc7e20c76
gersonmlb <gerson_159@outlook.es>
2020-07-25 20:40:43 -0500

    ULTIMOS

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,5 @@
 var contador = 0;
-var fin_contador = 85; // Tiempo en en el que deseas que redireccione la funcion.
+var fin_contador = 40; // Tiempo en en el que deseas que redireccione la funcion.
 var iniciado = false;
 
 function cuenta() {
