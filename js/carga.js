a54d90a5e8ff76f6235978fbeaf25a9888885f90
gersonmlb <gerson_159@outlook.es>
2020-07-25 21:46:38 -0500

    nuevos

--- js/carga.js
+++ js/carga.js
@@ -1,5 +1,5 @@
 var contador = 0;
-var fin_contador = 3; // Tiempo en en el que deseas que redireccione la funcion.
+var fin_contador = 2; // Tiempo en en el que deseas que redireccione la funcion.
 var iniciado = false;
 
 function cuenta() {
